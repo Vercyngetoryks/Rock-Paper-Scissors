89df166b874ed164c2c08b27ba3d5721d78176cc
vercyngetoryks <michal.krakowinski@gmail.com>
2024-10-02 11:14:09 +0200

    create second version of game logic

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function getComputerChoice() {
-  const number = Math.floor(Math.random() * 3) + 1;
-  if (number === 1) console.log("Rock");
-  else if (number === 2) console.log("Paper");
-  else console.log("Scisors");
+  const number = Math.floor(Math.random() * 3);
+  ////////////////////////////////////
+  // First version
+  ////////////////////////////////////
+  //   if (number === 0) console.log("Rock");
+  //   else if (number === 1) console.log("Paper");
+  //   else console.log("Scisors");
+
+  ////////////////////////////////////
+  // Second version
+  ////////////////////////////////////
+  const figure = ["Rock", "Paper", "Scisors"];
+  console.log(figure[number]);
 }
 
 getComputerChoice();
