0989910f6f5436383437a0fcc277a437624d3791
vercyngetoryks <michal.krakowinski@gmail.com>
2024-10-21 16:37:57 +0200

    Add announce a winner of the game once one player reaches 5 points.

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const figure = ["Rock", "Paper", "Scisors"];
 const buttons = document.querySelectorAll(".btn");
 const gameWindow = document.querySelector(".game");
+const results = document.createElement("div");
+results.classList.add("results");
+const humanChoice = document.createElement("p");
+const computerChoice = document.createElement("p");
+const roundResult = document.createElement("p");
+const gameScore = document.createElement("p");
+results.appendChild(humanChoice);
+results.appendChild(computerChoice);
+results.appendChild(roundResult);
+results.appendChild(gameScore);
+gameWindow.appendChild(results);
 let human = "";
 let comp = "";
 let humanScore = 0;
@@ -10,16 +21,23 @@ let count = 0;
 function getComputerChoice() {
   const number = Math.floor(Math.random() * 3);
   comp = figure[number];
-  console.log(`Computer choose: ${comp}`);
+}
+
+function endGame() {
+  buttons.forEach((button) => button.removeEventListener("click", playRound));
+  const finalResult = document.createElement("p");
+  results.appendChild(finalResult);
+  if (humanScore > computerScore) finalResult.textContent = "You Win!";
+  else finalResult.textContent = "You loose!";
 }
 
 function playRound() {
   human = this.textContent;
-  console.log(`You choose: ${human}`);
   getComputerChoice();
   humanChoice.textContent = `You choose: ${human}`;
   computerChoice.textContent = `Computer choose: ${comp}`;
-  if (human === comp) console.log("Draw");
+
+  if (human === comp) roundResult.textContent = "Draw";
   else if (
     (human === "Rock" && comp === "Paper") ||
     (human === "Paper" && comp === "Scisors") ||
@@ -33,34 +51,10 @@ function playRound() {
   }
   gameScore.textContent = `Score - You: ${humanScore}, Computer: ${computerScore}`;
   count++;
-}
 
-function updateChoices() {
-  humanChoice.textContent = `You choose: ${human}`;
-  computerChoice.textContent = `Computer choose: ${comp}`;
+  if (humanScore === 5 || computerScore === 5) {
+    endGame();
+  }
 }
 
 buttons.forEach((button) => button.addEventListener("click", playRound));
-
-const results = document.createElement("div");
-results.classList.add("results");
-const humanChoice = document.createElement("p");
-const computerChoice = document.createElement("p");
-const roundResult = document.createElement("p");
-const gameScore = document.createElement("p");
-results.appendChild(humanChoice);
-results.appendChild(computerChoice);
-results.appendChild(roundResult);
-results.appendChild(gameScore);
-gameWindow.appendChild(results);
-
-// function playGame() {
-//   while (count < 5) {
-//     playRound(getHumanChoice(), getComputerChoice());
-//   }
-//   if (humanScore > computerScore) console.log("You Win!");
-//   else if (humanScore < computerScore) console.log("You loose!");
-//   else console.log("The Game is a Draw!");
-// }
-
-// playGame();
