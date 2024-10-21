9702265f1ab23b39955783f06cf2c606db1ba2b7
vercyngetoryks <michal.krakowinski@gmail.com>
2024-10-21 15:13:59 +0200

    Add a div for displaying results and change all of console.logs into DOM methods.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const figure = ["Rock", "Paper", "Scisors"];
 const buttons = document.querySelectorAll(".btn");
+const gameWindow = document.querySelector(".game");
 let human = "";
 let comp = "";
 let humanScore = 0;
@@ -16,23 +17,43 @@ function playRound() {
   human = this.textContent;
   console.log(`You choose: ${human}`);
   getComputerChoice();
+  humanChoice.textContent = `You choose: ${human}`;
+  computerChoice.textContent = `Computer choose: ${comp}`;
   if (human === comp) console.log("Draw");
   else if (
     (human === "Rock" && comp === "Paper") ||
     (human === "Paper" && comp === "Scisors") ||
     (human === "Scisors" && comp === "Rock")
   ) {
-    console.log(`You lose! ${comp} beats ${human}`);
+    roundResult.textContent = `You lose! ${comp} beats ${human}`;
     computerScore++;
   } else {
-    console.log(`You win! ${human} beats ${comp}`);
+    roundResult.textContent = `You win! ${human} beats ${comp}`;
     humanScore++;
   }
-  console.log(`Score - You: ${humanScore}, Computer: ${computerScore}`);
+  gameScore.textContent = `Score - You: ${humanScore}, Computer: ${computerScore}`;
   count++;
 }
 
+function updateChoices() {
+  humanChoice.textContent = `You choose: ${human}`;
+  computerChoice.textContent = `Computer choose: ${comp}`;
+}
+
 buttons.forEach((button) => button.addEventListener("click", playRound));
+
+const results = document.createElement("div");
+results.classList.add("results");
+const humanChoice = document.createElement("p");
+const computerChoice = document.createElement("p");
+const roundResult = document.createElement("p");
+const gameScore = document.createElement("p");
+results.appendChild(humanChoice);
+results.appendChild(computerChoice);
+results.appendChild(roundResult);
+results.appendChild(gameScore);
+gameWindow.appendChild(results);
+
 // function playGame() {
 //   while (count < 5) {
 //     playRound(getHumanChoice(), getComputerChoice());
