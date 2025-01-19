705067f6c9d90422439cc3786e9df9476f18ab76
vercyngetoryks <michal.krakowinski.gmail.com>
2025-01-19 10:51:49 +0100

    add simple UI and some more features into game logic

--- script.js
+++ script.js
@@ -2,9 +2,10 @@
 
 function gameController() {
   const choices = ["Rock", "Paper", "Scissors"];
+
   let humanScore = 0;
   let computerScore = 0;
-  let count = 0;
+  const checkWin = () => humanScore === 5 || computerScore === 5;
 
   const computerChoice = () =>
     choices[Math.floor(Math.random() * choices.length)];
@@ -22,13 +23,6 @@ function gameController() {
       humanScore++;
       return "human";
     }
-
-    // gameScore.textContent = `Score - You: ${humanScore}, Computer: ${computerScore}`;
-    // count++;
-
-    // if (humanScore === 5 || computerScore === 5) {
-    //   endGame();
-    // }
   }
 
   const getHumanScore = () => humanScore;
@@ -39,6 +33,7 @@ function gameController() {
     computerChoice,
     getHumanScore,
     getComputerScore,
+    checkWin,
   };
 }
 
@@ -48,48 +43,54 @@ function screenController() {
   const results = document.createElement("div");
   results.classList.add("results");
   results.innerHTML = `
-    <p class="human-choice"></p>
-    <p class="computer-choice"></p>
-    <p class="round-result"></p>
-    <p class="game-score"></p>`;
+  <p class="human-choice"></p>
+  <p class="computer-choice"></p>
+  <p class="round-result"></p>
+  <p class="game-score"></p>`;
   gameWindow.appendChild(results);
   const humanChoiceText = results.querySelector(".human-choice");
   const computerChoiceText = results.querySelector(".computer-choice");
   const roundResultText = results.querySelector(".round-result");
   const gameScoreText = results.querySelector(".game-score");
+
   function updateScreen(e) {
     const humanChoice = e.target.textContent;
     const computerChoice = game.computerChoice();
     const result = game.playRound(humanChoice, computerChoice);
-    humanChoiceText.textContent = `You choose ${humanChoice}`;
-    computerChoiceText.textContent = `Computer choose ${computerChoice}`;
+    humanChoiceText.textContent = `You choose: ${humanChoice}`;
+    computerChoiceText.textContent = `Computer choose: ${computerChoice}`;
     roundResultText.textContent =
       result === "draw"
         ? "It's a draw!"
         : result === "comp"
         ? "You loose!"
         : "You win!!!";
-    gameScoreText.textContent = `You: ${game.getHumanScore()} | Computer: ${game.getComputerScore()}`;
+    gameScoreText.textContent = `Score: You: ${game.getHumanScore()} | Computer: ${game.getComputerScore()}`;
   }
 
-  gameWindow.addEventListener("click", (e) => {
+  function handleClick(e) {
     if (e.target.classList.contains("btn")) {
       updateScreen(e);
+      if (game.checkWin()) {
+        gameWindow.removeEventListener("click", handleClick);
+        const finalResult = document.createElement("p");
+        const newGame = document.createElement("button");
+        newGame.classList.add("new-game");
+        results.appendChild(finalResult);
+        results.appendChild(newGame);
+        finalResult.textContent =
+          game.getHumanScore() > game.getComputerScore()
+            ? "You win!!!"
+            : "You loose!";
+        newGame.textContent = "New game";
+        newGame.addEventListener("click", () => {
+          location.reload();
+        });
+      }
     }
-  });
+  }
+
+  gameWindow.addEventListener("click", handleClick);
 }
 
 screenController();
-
-// function clickEventHandler() {
-//   const buttons = document.querySelectorAll(".btn");
-//   buttons.forEach((button) => button.addEventListener("click", playRound));
-
-//   function endGame() {
-//     buttons.forEach((button) => button.removeEventListener("click", playRound));
-//     const finalResult = document.createElement("p");
-//     results.appendChild(finalResult);
-//     if (humanScore > computerScore) finalResult.textContent = "You Win!";
-//     else finalResult.textContent = "You loose!";
-//   }
-// }
